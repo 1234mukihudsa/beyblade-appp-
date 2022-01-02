e261c995625df735688bad69579144714aaf81fb
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2022-01-01 18:41:54 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,24 @@ function take_snapshot(){
  
 
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/P3hfc9W4K/model.json', modelLoaded());
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/MqwAhgSp_/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log('modelLoaded');
-};
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if( error){
+        console.error(error);
+    }
+    else{
+       console.log(results);
+       document.getElementById("result_object_name").innerHTML = results[0].label;
+       document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2);
+    }
+}
 
